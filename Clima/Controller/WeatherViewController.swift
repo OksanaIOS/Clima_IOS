2742298690831c17d7b7c7729d7edc92312869e5
Oksana ogay <example@email.com>
2024-10-10 15:37:25 +0500

    commit

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -7,18 +7,76 @@
 //
 
 import UIKit
+import SnapKit
 
 class WeatherViewController: UIViewController {
-
-    @IBOutlet weak var conditionImageView: UIImageView!
-    @IBOutlet weak var temperatureLabel: UILabel!
-    @IBOutlet weak var cityLabel: UILabel!
     
+
+//    @IBOutlet weak var conditionImageView: UIImageView!
+//    @IBOutlet weak var temperatureLabel: UILabel!
+//    @IBOutlet weak var cityLabel: UILabel!
+    
+    private lazy var mainStackView: UIStackView  = {
+        let element = UIStackView()
+        element.axis = .vertical
+        element.backgroundColor = .blue
+        element.distribution = .fillProportionally
+        
+        element.translatesAutoresizingMaskIntoConstraints = false
+        return element
+    }()
+    
+    private lazy var backgroundImageView: UIImageView  = {
+        let element = UIImageView()
+        element.image = UIImage(named: Constants.background)
+        element.contentMode = .scaleAspectFill
+        
+        
+        element.translatesAutoresizingMaskIntoConstraints = false
+        return element
+    }()
+    
+    private lazy var headerStackView: UIStackView  = {
+        let element = UIStackView()
+        element.axis = .horizontal
+        
+        element.translatesAutoresizingMaskIntoConstraints = false
+        return element
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setViews()
+        setupConstraints()
+        
     }
 
 
 }
 
+extension WeatherViewController {
+    private func setViews() {
+        view.addSubview(backgroundImageView)
+        view.addSubview(mainStackView)
+        
+    }
+    
+    private func setupConstraints() {
+       
+        
+        backgroundImageView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        mainStackView.snp.makeConstraints { make in
+            make.edges.equalTo(view.safeAreaLayoutGuide).inset(24)
+            
+        }
+        
+    }
+    
+}
+
+
+#Preview {
+    WeatherViewController()
+}
---
+++ Clima/Model/Constants.swift
@@ -0,0 +1,20 @@
+//
+//  Constants.swift
+//  Clima
+//
+//  Created by Vladimir on 10/10/24.
+//  Copyright © 2024 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+
+struct Constants {
+    static let background = "background"
+    static let weatherColor = "weatherColor"
+    static let celsius = "C"
+    static let searchSF = "magnifyingglass"
+    static let geoSF = "location.circle.fill"
+    static let search = "Search"
+    static let coditionSF = "cloud.rain"
+}
